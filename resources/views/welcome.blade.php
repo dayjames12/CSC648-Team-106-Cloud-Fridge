47ece8f8d10dfb419524fd8287d6e774b98e54e2
George Butler <georgebutleriv@gmail.com>
2019-11-03 11:27:03 -0800

    Added Bulma so frontend team can integrate it.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="title">Laravel</h1>
-</div>
+<section class="hero is-primary">
+  <div class="hero-head">
+    <navbar-component></navbar-component>
+  </div>
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">Cloud Fridge</h1>
+      <h2 class="subtitle">The easiest way to manage your fridge.</h2>
+    </div>
+  </div>
+</section>
 @endsection
