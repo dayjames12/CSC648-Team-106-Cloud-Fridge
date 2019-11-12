8a09ebc0a32bd92f394177006237bdf322e36505
Raymond W Gee <rgee5@mail.sfsu.edu>
2019-11-11 22:03:56 -0800

    added ternary to tiles; red if expiring in 3 days, normal teal otherwise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
 {{-- enables usage of bulma icons --}}
 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
+
 <section class="section">
     <div class="container">
 
@@ -85,8 +86,11 @@
                         <div class="tile is-parent is-vertical">
 
                             @foreach ($items as $item)
-                            <article class="tile is-child notification is-primary">
-                                
+                            {{-- Ternary. If exp date is less than 3 days, red tile. Otherwise, normal teal  --}}
+                            <article class="title is-child notification {{ 
+                                (Carbon\Carbon::now()->diffInDays(Carbon\Carbon::parse($item->expiration)->format('Y-m-d'))) < 3
+                                ? 'is-danger' : 'is-primary' }}">
+
                                 {{-- Retrieve item id to delete when button is pressed --}}
                                 
                                 <form method="POST" action="/items/{{ $item->id }}">
