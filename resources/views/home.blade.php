ea622f542cc281309eb82447bb097766491c356d
Jian Qi <jianqi1997@gmail.com>
2019-11-21 00:20:16 -0800

    href link pages of nav bar at bottom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -156,15 +156,15 @@
                     </span>
                     <p class="is-size-7">Inventory</p>
                 </a>
-                <a class="navbar-item is-expanded is-block has-text-centered">
+                <a class="navbar-item is-expanded is-block has-text-centered" href= "/groceryList">
                     <i class="fa fa-list"></i>
                     <p class="is-size-7">Grocery List</p>
                 </a>
-                <a class="navbar-item is-expanded is-block has-text-centered">
+                <a class="navbar-item is-expanded is-block has-text-centered" href= "/recipe">
                     <i class="fa fa-book"></i>
                     <p class="is-size-7">Recipe</p>
                 </a>
-                <a class="navbar-item is-expanded is-block has-text-centered">
+                <a class="navbar-item is-expanded is-block has-text-centered" href= "/foodList">
                     <i class="fa fa-cookie-bite"></i>
                     <p class="is-size-7">Food List</p>
                 </a>
