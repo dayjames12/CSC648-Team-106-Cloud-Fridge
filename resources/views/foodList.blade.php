ab0f15067a1049313d6419d1e972f1c8c2dbda44
Aang <aunghain123@gmail.com>
2019-12-03 13:48:00 -0800

    Food list update

--- resources/views/foodList.blade.php
+++ resources/views/foodList.blade.php
@@ -12,78 +12,88 @@
     
     <div class="block"> 
         <div class="container">
-        <nav class="level">
-            <div class="level-item has-text-centered">
-                <div class="action-buttons">
-                    <a class= "button is-link">
-                        <i class="fa fa-chevron-left">
-                        </i>
-                    </a>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                    <h2 class="title">Thursday</h2>
-                    <p class="h1">Oct 22</p>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div class="action-buttons">
-                    <a class= "button is-link">
-                        <i class="fa fa-chevron-right">
-                        </i>
-                    </a>
-                </div>
-            </div>
-           
-        </nav>
-
-        </div>
-
-        <div class="container has-text-centered">
-            <div class="notification" style="border-radius: 25px;">
-                <!-- <div class ="tile is-ancestor">
-                    <div class="tile is-parent">
-                        <div class= "tile is-child box">
-                            <p>Tile 1</p>
+            <div class="notification" style="background:#b0e5ff; border-radius:15px;">
+                <nav class="level">
+                    <div class="level-item has-text-centered">
+                        <div class="action-buttons" >
+                            <a class= "button is-link" style="background: transparent; border: none;">
+                                <i class="fa fa-chevron-left">
+                                </i>
+                            </a>
                         </div>
-
-                        
-                        
                     </div>
-                    <div class="tile is-parent">
-                        <div class= "tile is-child box">
-                            <div class="">
-                                <p>Tile 2</p>
+                    <div class="level-item has-text-centered">
+                        <div>
+                            <h2 class="title" style="color:white;">Thursday</h2>
+                            <p class="h1" style="color:white;">Oct 22</p>
+                        </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                        <div class="action-buttons">
+                            <a class= "button is-link" style="background: transparent; border: none;">
+                                <i class="fa fa-chevron-right">
+                                </i>
+                            </a>
+                        </div>
+                    </div>
+                
+                </nav>
+
+            </div>
+
+            <div class="container has-text-centered" style="padding-top: 30px;">
+                <div class="container" style="border-radius: 25px; padding-bottom: 5%;">
+                    <!-- <div class ="tile is-ancestor">
+                        <div class="tile is-parent">
+                            <div class= "tile is-child box">
+                                <p>Tile 1</p>
+                            </div>
+
+                            
+                            
+                        </div>
+                        <div class="tile is-parent">
+                            <div class= "tile is-child box">
+                                <div class="">
+                                    <p>Tile 2</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                
-                </div> -->
-                <div class="columns is-centered is-vcentered">
-                    <div class="column is-half">
-                        <div>
-                            <h1 class="title is-2"> Testing something</h1>
+                    
+                    </div> -->
+                    <div class="columns is-centered is-vcentered">
+                        <div class="column is-half">
+                            <div>
+                                <h2 class="title is-2" style="color:rgb(91, 192, 222);"> Today: </h2>
+                            </div>
                         </div>
-                    </div>
-                    <div class="column is-half is-narrow">
-                        <div class="placeHolder" >
-                            <figure class="image is-128x128">
-                                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                            </figure>
+                        <div class="column is-half is-narrow" style="padding-left: 20%;">
+                            <div class="placeHolder" >
+                                <figure class="image is-128x128">
+                                    {{-- Just a placeholder for now --}}
+                                    <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                                </figure>
+                            </div>
                         </div>
+
+
                     </div>
-
-
 
 
                 </div>
 
 
-            </div>
-        </div>
+                <div class="notification">
+                        Hello
+                        
+            
+                </div>
 
+            </div>
+        
+
+        </div>
 
 
 
