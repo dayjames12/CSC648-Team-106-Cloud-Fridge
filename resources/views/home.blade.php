0c67e959b930d535e27b98cdac7b9fb8f8c0471f
Raymond W Gee <rgee5@mail.sfsu.edu>
2019-12-17 11:51:08 -0800

    added routing for the new bottom item on nav bar (receipt upload)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -251,7 +251,7 @@
                     <i class="fa fa-cookie-bite"></i>
                     <p class="is-size-7">Food List</p>
                 </a>
-                <a class="navbar-item is-expanded is-block has-text-centered" href="">
+                <a class="navbar-item is-expanded is-block has-text-centered" href="/receiptUpload">
                     <i class="fas fa-receipt"></i>
                     <p class="is-size-7">Receipt Upload</p>
                 </a>
