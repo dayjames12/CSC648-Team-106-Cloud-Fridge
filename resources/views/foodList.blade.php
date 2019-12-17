205adee7bd9439f7b8928daa6be45ba10398b27a
Raymond W Gee <rgee5@mail.sfsu.edu>
2019-12-17 12:15:31 -0800

    minor spacing changes

--- resources/views/foodList.blade.php
+++ resources/views/foodList.blade.php
@@ -6,10 +6,7 @@
 {{-- enables usage of bulma icons --}}
 <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
-
 <section class="section">
-
-
     <div class="block">
         <div class="container">
             <div id="foodlist_date" class="notification" style="background:#b0e5ff; border-radius:15px;">
@@ -76,20 +73,13 @@
                                 </figure>
                             </div>
                         </div>
-
-
                     </div>
-
 
                 </div>
 
-               
             </div>
 
-
         </div>
-
-
 
     </div>
 
@@ -103,93 +93,92 @@
         <div class="modal-background"></div>
         <div class="modal-card">
             <header class="modal-card-head">
-            <p class="modal-card-title">Grocery</p>
-            <button class="delete" aria-label="close"></button>
+                <p class="modal-card-title">Grocery</p>
+                <button class="delete" aria-label="close"></button>
             </header>
             <section class="modal-card-body">
                 <aside>
                     <h1>Groceries</h1>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Salad greens</span>
+                        <input type=checkbox>
+                        <span>Salad greens</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Button Mushrooms</span>
+                        <input type=checkbox>
+                        <span>Button Mushrooms</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Cherry Tomatoes</span>
+                        <input type=checkbox>
+                        <span>Cherry Tomatoes</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Onion</span>
+                        <input type=checkbox>
+                        <span>Onion</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Peppers</span>
+                        <input type=checkbox>
+                        <span>Peppers</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Cucumber</span>
+                        <input type=checkbox>
+                        <span>Cucumber</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Naan Bread</span>
+                        <input type=checkbox>
+                        <span>Naan Bread</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Feta Cheese</span>
+                        <input type=checkbox>
+                        <span>Feta Cheese</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Food Colouring</span>
+                        <input type=checkbox>
+                        <span>Food Colouring</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Wooden toothpicks</span>
+                        <input type=checkbox>
+                        <span>Wooden toothpicks</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Wooden Skewers</span>
+                        <input type=checkbox>
+                        <span>Wooden Skewers</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Pickles</span>
+                        <input type=checkbox>
+                        <span>Pickles</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Pickled Veggies</span>
+                        <input type=checkbox>
+                        <span>Pickled Veggies</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Olives</span>
+                        <input type=checkbox>
+                        <span>Olives</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Croutons</span>
+                        <input type=checkbox>
+                        <span>Croutons</span>
                     </label>
 
                     <label>
-                    <input type=checkbox>
-                    <span>Nice Soup</span>
+                        <input type=checkbox>
+                        <span>Nice Soup</span>
                     </label>
-
                 </aside>
             </section>
             <footer class="modal-card-foot">
@@ -219,8 +208,8 @@
                 <p class="is-size-7">Food List</p>
             </a>
             <a class="navbar-item is-expanded is-block has-text-centered" href="/receiptUpload">
-                    <i class="fas fa-receipt"></i>
-                    <p class="is-size-7">Receipt Upload</p>
+                <i class="fas fa-receipt"></i>
+                <p class="is-size-7">Receipt Upload</p>
             </a>
         </div>
     </nav>
